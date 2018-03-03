7b41aa411335dc62f886049eab1480b3f0b9af02
kekote169 <36438413+kekote169@users.noreply.github.com>
2018-03-03 18:49:25 +0100

    Add files via upload
    Lista de Elementos con Atributos

---
+++ ListaConArray/pakiman.js
@@ -0,0 +1,22 @@
+class Pakiman {
+  constructor(n, v, a) {
+    this.imagen = new Image();
+    this.nombre = n;
+    this.vida = v;
+    this.ataque = a;
+
+    this.imagen.src = imagenes[this.nombre];
+  }
+  hablar(){
+    alert(this.nombre);
+  }
+  mostrar(){
+    document.body.appendChild(this.imagen);
+    document.write("<p>");
+      document.write("<strong>" + this.nombre + "</strong><br />");
+      document.write("Vida: " + this.vida + "<br />");
+      document.write("ataque: " + this.ataque + "<br />");
+      document.write("<hr />");
+    document.write("</p>");
+  }
+}
